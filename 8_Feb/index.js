1a551d730b070d0f0eed29fe0150e7b7ee5bbd52
MohammedFaizUrRahman89 <mohammedfaizurrahman89@gmail.com>
2025-02-08 20:14:20 +0530

    8_Feb Notes
    js assignments

---
+++ 8_Feb/index.js
@@ -0,0 +1,39 @@
+// 8 feb
+
+
+//Write a Fuction to Reverse a String
+// let animals = ["Lion","Tiger","Bear","Leapord","Monkey"];
+// console.log(animals.reverse());
+
+
+//Check if a string is a palindrome (reads the same backward and forward)
+// let palindrome = Str =>
+// Str===Str.split('').reverse().join('');
+// console.log(palindrome("madam"));
+// console.log(palindrome("Hello"));
+
+
+//Count the number of vowels in a string
+// let vowels = str =>
+// (str.match(/[aeiou]/gi) || []).length;
+// console.log(vowels("Hello World"));
+// console.log(vowels("irfan didnt done assignment"));
+
+
+// Find the first non-reapeated character in a string
+// function UniqueChar(str) {
+//     for(let char of str) {
+//         if (str.indexOf(char) === str.lastIndexOf(char)) {
+//             return char;
+//         }
+//     }
+//     return null;
+// }
+// console.log(UniqueChar("ibreem"));
+// console.log(UniqueChar("Lords"));
+
+
+// Capitalize the first letter of each word in a sentence
+// let sentence = "mr unknown";
+// let Capitalize = sentence.replace(/\b\w/g, char => char.toUpperCase());
+// console.log(Capitalize);
